c06b523c863cfba97f9496353b06b39ff84d7c1e
newak <newakshayjangir@gmail.com>
2021-07-01 20:55:30 +0530

    logo and slash screen

--- app/src/main/java/com/ak_applications/astrosolution/splash_screen.kt
+++ app/src/main/java/com/ak_applications/astrosolution/splash_screen.kt
@@ -1,8 +1,23 @@
 package com.ak_applications.astrosolution
 
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 
 class splash_screen: AppCompatActivity()
 {
+    private lateinit var  SplashHandler: Handler
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.splash_screen_activity)
+
+        SplashHandler = Handler(Looper.getMainLooper())
+        SplashHandler.postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        },3000)
+    }
 
 }
