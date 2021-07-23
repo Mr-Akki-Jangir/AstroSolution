96e30e0fb90f9ff449c8bc8b4264811a57dea083
newak <newakshayjangir@gmail.com>
2021-07-23 21:29:50 +0530

    Removed all errors and 99% warning just one splash screen image has two very long path which will take too much time to optimize.

--- app/src/main/java/com/ak_applications/astrosolution/MainActivity.kt
+++ app/src/main/java/com/ak_applications/astrosolution/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.ak_applications.astrosolution
 
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.os.Handler
-import android.os.StrictMode
-import android.view.LayoutInflater
+import android.os.Looper
+//import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
-import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.RecyclerView
@@ -31,52 +31,78 @@ import kotlin.collections.ArrayList
 import kotlin.math.abs
 
 
-public class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var viewPager2: ViewPager2
-    private val sliderHandler = Handler()
-    lateinit var card1: MaterialCardView
-    lateinit var AppBarNavIcon: ActionBarDrawerToggle
-    lateinit var HomeDLyt: DrawerLayout
+    private val sliderHandler = Handler(Looper.getMainLooper())
+    private lateinit var card1: MaterialCardView
+    lateinit var appBarNavIcon: ActionBarDrawerToggle
+    private lateinit var homeDLyt: DrawerLayout
 
-    private lateinit var CustomFormMaterialDialogLayout: View
-    private lateinit var CustomFormMaterialDialog: MaterialAlertDialogBuilder
+    private lateinit var customFormAlertDialogLayout: View
+    private lateinit var customFormDialog: MaterialAlertDialogBuilder
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                Toast.makeText(this, "Night Mode Enabled",Toast.LENGTH_LONG).show()
+            }
+            Configuration.UI_MODE_NIGHT_NO -> {
+                Toast.makeText(this, "No Night Mode",Toast.LENGTH_LONG).show()
+            }
+        }
 
         viewPager2 = findViewById(R.id.h_slider)
-        val HNav: NavigationView = findViewById(R.id.hNavView)
-        HomeDLyt = findViewById(R.id.h_drawerlyt)
+        val hNav: NavigationView = findViewById(R.id.hNavView)
+        homeDLyt = findViewById(R.id.hDrawerLayout)
         card1 = findViewById(R.id.card1)
-        val TopAppBar: MaterialToolbar = findViewById(R.id.h_src_topAb)
+        val topAppBar: MaterialToolbar = findViewById(R.id.h_src_topAb)
+        val card2: LinearLayout = findViewById(R.id.card2)
+        val card3: LinearLayout = findViewById(R.id.card3)
+        val card4: LinearLayout = findViewById(R.id.card4)
+        val card5: LinearLayout = findViewById(R.id.card5)
+        val card6: LinearLayout = findViewById(R.id.card6)
+        val card7: LinearLayout = findViewById(R.id.card7)
+        val card8: LinearLayout = findViewById(R.id.card8)
+        val card9: LinearLayout = findViewById(R.id.card9)
 
-        setSupportActionBar(TopAppBar)
-        AppBarNavIcon = ActionBarDrawerToggle(this, HomeDLyt, R.string.NOpen, R.string.NClose)
-        AppBarNavIcon.syncState()
+
+
+        setSupportActionBar(topAppBar)
+        appBarNavIcon = ActionBarDrawerToggle(this, homeDLyt, R.string.NOpen, R.string.NClose)
+        appBarNavIcon.syncState()
 
 
          card1.setOnClickListener(this)
-        CustomFormMaterialDialog = MaterialAlertDialogBuilder(this)
+         card2.setOnClickListener(this)
+         card3.setOnClickListener(this)
+         card4.setOnClickListener(this)
+         card5.setOnClickListener(this)
+         card6.setOnClickListener(this)
+         card7.setOnClickListener(this)
+         card8.setOnClickListener(this)
+         card9.setOnClickListener(this)
+        customFormDialog = MaterialAlertDialogBuilder(this)
 
-        HNav.setNavigationItemSelectedListener {
+        hNav.setNavigationItemSelectedListener {
             it.isChecked = true
-            HomeDLyt.closeDrawer(GravityCompat.START)
+            homeDLyt.closeDrawer(GravityCompat.START)
             true
 
         }
 
-        val sliderh_slider_item: MutableList<h_slider_item> = ArrayList()
-        sliderh_slider_item.add(h_slider_item(R.drawable.a1))
-        sliderh_slider_item.add(h_slider_item(R.drawable.a2))
-        sliderh_slider_item.add(h_slider_item(R.drawable.slide2))
-        sliderh_slider_item.add(h_slider_item(R.drawable.slide3))
+        val sliderHSliderItem: MutableList<h_slider_item> = ArrayList()
+        sliderHSliderItem.add(h_slider_item(R.drawable.a1))
+        sliderHSliderItem.add(h_slider_item(R.drawable.a2))
+        sliderHSliderItem.add(h_slider_item(R.drawable.slide2))
+        sliderHSliderItem.add(h_slider_item(R.drawable.slide3))
 
-        viewPager2.adapter = h_slider_adapter(sliderh_slider_item, viewPager2)
+        viewPager2.adapter = h_slider_adapter(sliderHSliderItem, viewPager2)
 
         viewPager2.clipToPadding = false
         viewPager2.clipChildren = false
@@ -104,7 +130,7 @@ public class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (AppBarNavIcon.onOptionsItemSelected(item))
+        if (appBarNavIcon.onOptionsItemSelected(item))
             return true
         return super.onOptionsItemSelected(item)
     }
@@ -128,9 +154,90 @@ public class MainActivity : AppCompatActivity(), View.OnClickListener {
         when (v?.id) {
 
             R.id.card1 -> {
-                CustomFormMaterialDialogLayout =
-                    LayoutInflater.from(this).inflate(R.layout.custom_dialog_form_lyt, null, false)
-                LaunchCustomFromMaterialDialog()
+                customFormAlertDialogLayout =
+                    View.inflate(this, R.layout.custom_dialog_form_lyt, null)
+//                    LayoutInflater.from(this).inflate(R.layout.custom_dialog_form_lyt, null, false)
+                launchCustomFromMaterialDialog()
+//                val policy: StrictMode.ThreadPolicy =
+//                    StrictMode.ThreadPolicy.Builder().permitAll().build()
+//                StrictMode.setThreadPolicy(policy)
+            }
+
+            R.id.card2 -> {
+                customFormAlertDialogLayout =
+                    View.inflate(this, R.layout.custom_dialog_form_lyt, null)
+//                    LayoutInflater.from(this).inflate(R.layout.custom_dialog_form_lyt, null, false)
+                launchCustomFromMaterialDialog()
+//                val policy: StrictMode.ThreadPolicy =
+//                    StrictMode.ThreadPolicy.Builder().permitAll().build()
+//                StrictMode.setThreadPolicy(policy)
+            }
+
+            R.id.card3 -> {
+                customFormAlertDialogLayout =
+                    View.inflate(this, R.layout.custom_dialog_form_lyt, null)
+//                    LayoutInflater.from(this).inflate(R.layout.custom_dialog_form_lyt, null, false)
+                launchCustomFromMaterialDialog()
+//                val policy: StrictMode.ThreadPolicy =
+//                    StrictMode.ThreadPolicy.Builder().permitAll().build()
+//                StrictMode.setThreadPolicy(policy)
+            }
+
+            R.id.card4 -> {
+                customFormAlertDialogLayout =
+                    View.inflate(this, R.layout.custom_dialog_form_lyt, null)
+//                    LayoutInflater.from(this).inflate(R.layout.custom_dialog_form_lyt, null, false)
+                launchCustomFromMaterialDialog()
+//                val policy: StrictMode.ThreadPolicy =
+//                    StrictMode.ThreadPolicy.Builder().permitAll().build()
+//                StrictMode.setThreadPolicy(policy)
+            }
+
+            R.id.card5 -> {
+                customFormAlertDialogLayout =
+                    View.inflate(this, R.layout.custom_dialog_form_lyt, null)
+//                    LayoutInflater.from(this).inflate(R.layout.custom_dialog_form_lyt, null, false)
+                launchCustomFromMaterialDialog()
+//                val policy: StrictMode.ThreadPolicy =
+//                    StrictMode.ThreadPolicy.Builder().permitAll().build()
+//                StrictMode.setThreadPolicy(policy)
+            }
+
+            R.id.card6 -> {
+                customFormAlertDialogLayout =
+                    View.inflate(this, R.layout.custom_dialog_form_lyt, null)
+//                    LayoutInflater.from(this).inflate(R.layout.custom_dialog_form_lyt, null, false)
+                launchCustomFromMaterialDialog()
+//                val policy: StrictMode.ThreadPolicy =
+//                    StrictMode.ThreadPolicy.Builder().permitAll().build()
+//                StrictMode.setThreadPolicy(policy)
+            }
+
+            R.id.card7 -> {
+                customFormAlertDialogLayout =
+                    View.inflate(this, R.layout.custom_dialog_form_lyt, null)
+//                    LayoutInflater.from(this).inflate(R.layout.custom_dialog_form_lyt, null, false)
+                launchCustomFromMaterialDialog()
+//                val policy: StrictMode.ThreadPolicy =
+//                    StrictMode.ThreadPolicy.Builder().permitAll().build()
+//                StrictMode.setThreadPolicy(policy)
+            }
+
+            R.id.card8 -> {
+                customFormAlertDialogLayout =
+                    View.inflate(this, R.layout.custom_dialog_form_lyt, null)
+//                    LayoutInflater.from(this).inflate(R.layout.custom_dialog_form_lyt, null, false)
+                launchCustomFromMaterialDialog()
+//                val policy: StrictMode.ThreadPolicy =
+//                    StrictMode.ThreadPolicy.Builder().permitAll().build()
+//                StrictMode.setThreadPolicy(policy)
+            }
+
+            R.id.card9 -> {
+                customFormAlertDialogLayout =
+                    View.inflate(this, R.layout.custom_dialog_form_lyt, null)
+//                    LayoutInflater.from(this).inflate(R.layout.custom_dialog_form_lyt, null, false)
+                launchCustomFromMaterialDialog()
 //                val policy: StrictMode.ThreadPolicy =
 //                    StrictMode.ThreadPolicy.Builder().permitAll().build()
 //                StrictMode.setThreadPolicy(policy)
@@ -138,38 +245,38 @@ public class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    private fun LaunchCustomFromMaterialDialog() {
-        val CDFName = CustomFormMaterialDialogLayout.findViewById<TextInputEditText>(R.id.cfdfn)
-        val CDFPhoneNo = CustomFormMaterialDialogLayout.findViewById<TextInputEditText>(R.id.cfdpn)
-        val CDFProblem =
-            CustomFormMaterialDialogLayout.findViewById<TextInputEditText>(R.id.cfdpblm)
+    private fun launchCustomFromMaterialDialog() {
+        val cDFName = customFormAlertDialogLayout.findViewById<TextInputEditText>(R.id.customFormDialogFullName)
+        val cDFPhoneNo = customFormAlertDialogLayout.findViewById<TextInputEditText>(R.id.customFormDialogPhoneNo)
+        val cDFProblem =
+            customFormAlertDialogLayout.findViewById<TextInputEditText>(R.id.customFormDialogProblem)
 
-        CustomFormMaterialDialog.setView(CustomFormMaterialDialogLayout)
-            .setPositiveButton("Send") { dialog, which ->
-                val MyMail: String = getString(R.string.FromEmail)
-                val MyPas: String = getString(R.string.FromEmailPass)
-                val UEmail: String =
-                    CDFName.text.toString() + CDFPhoneNo.text.toString() + CDFProblem.text.toString()
-                val prop: Properties = Properties()
-                prop.put("mail.smtp.auth", "true")
-                prop.put("mail.smtp.starttls.enable", "true")
-                prop.put("mail.smtp.host", "smtp.gmail.com")
-                prop.put("mail.smtp.port", "587")
+        customFormDialog.setView(customFormAlertDialogLayout)
+            .setPositiveButton("Send") { _, _ ->
+                val myMail: String = getString(R.string.FromEmail)
+                val myPas: String = getString(R.string.FromEmailPass)
+                val uEmail: String =
+                    cDFName.text.toString() + cDFPhoneNo.text.toString() + cDFProblem.text.toString()
+                val prop = Properties()
+                prop["mail.smtp.auth"] = "true"
+                prop["mail.smtp.starttls.enable"] = "true"
+                prop["mail.smtp.host"] = "smtp.gmail.com"
+                prop["mail.smtp.port"] = "587"
                 val session =
                     Session.getInstance(prop, object : Authenticator() {
                         override fun getPasswordAuthentication(): PasswordAuthentication {
-                            return PasswordAuthentication(MyMail, MyPas)
+                            return PasswordAuthentication(myMail, myPas)
                         }
                     })
                 try {
                     val message: Message = MimeMessage(session)
-                    message.setFrom(InternetAddress(MyMail))
+                    message.setFrom(InternetAddress(myMail))
                     message.setRecipients(
                         Message.RecipientType.TO,
                         InternetAddress.parse(getString(R.string.ToMail))
                     )
-                    message.setSubject("Coming From App")
-                    message.setText(UEmail)
+                    message.subject = "Coming From App"
+                    message.setText(uEmail)
                     Transport.send(message)
                     Toast.makeText(applicationContext, "Send Success", Toast.LENGTH_LONG)
                         .show()
@@ -179,7 +286,7 @@ public class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
 
 
-            .setNegativeButton("Cancel") { dialog, which ->
+            .setNegativeButton("Cancel") { dialog, _ ->
                 dialog.dismiss()
             }.show()
     }
